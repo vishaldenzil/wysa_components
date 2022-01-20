f92da63e6935a11c09736db622b64b4293354839
w3docs <vishal@touchkin.com>
2022-01-20 10:56:12 +0530

    changes for the  icons

--- src/components/Toaster/index.js
+++ src/components/Toaster/index.js
@@ -1,10 +1,4 @@
 import React from 'react'
-
-import InfoIcon from '../Assets/info_logo.svg';
-import WarningIcon from "../Assets/toast-warning.svg";
-import ErrorIcon from '../Assets/toast-error.svg';
-import SuccessIcon from '../Assets/toast-success.svg';
-
 import './index.css';
 
 const ToasterComponent = ({
@@ -12,23 +6,6 @@ const ToasterComponent = ({
     removeToast
 }) => {
     let toastIcon;
-
-    switch (toast.type) {
-        case 'success':
-            toastIcon = SuccessIcon;
-            break;
-        case 'info':
-            toastIcon = InfoIcon;
-            break;
-        case 'error':
-            toastIcon = ErrorIcon;
-            break;
-        case 'warning':
-            toastIcon = WarningIcon;
-            break;
-        default:
-            break;
-    }
     return (
         <div
             key={toast.id}
@@ -36,7 +13,6 @@ const ToasterComponent = ({
             onClick={() => removeToast(toast.id)}
             className={`toast ${toast.type}_toast fade-in`}
         >
-            <img className={`${toast.type}_icon`} src={toastIcon} alt={`${toast.type} icon`} />
             <div className={`${toast.type}_text`}>
                 <strong>{toast.title}</strong>
                 <br />
