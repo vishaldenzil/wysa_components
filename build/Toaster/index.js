d7920e057390b660f465713e73956de22f02b349
w3docs <vishal@touchkin.com>
2022-01-20 11:06:31 +0530

    changes for icons issue

--- build/Toaster/index.js
+++ build/Toaster/index.js
@@ -7,14 +7,6 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _info_logo = _interopRequireDefault(require("../Assets/info_logo.svg"));
-
-var _toastWarning = _interopRequireDefault(require("../Assets/toast-warning.svg"));
-
-var _toastError = _interopRequireDefault(require("../Assets/toast-error.svg"));
-
-var _toastSuccess = _interopRequireDefault(require("../Assets/toast-success.svg"));
-
 require("./index.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -23,28 +15,6 @@ var ToasterComponent = function ToasterComponent(_ref) {
   var toast = _ref.toast,
       removeToast = _ref.removeToast;
   var toastIcon;
-
-  switch (toast.type) {
-    case 'success':
-      toastIcon = _toastSuccess.default;
-      break;
-
-    case 'info':
-      toastIcon = _info_logo.default;
-      break;
-
-    case 'error':
-      toastIcon = _toastError.default;
-      break;
-
-    case 'warning':
-      toastIcon = _toastWarning.default;
-      break;
-
-    default:
-      break;
-  }
-
   return /*#__PURE__*/_react.default.createElement("div", {
     key: toast.id,
     role: "presentation",
@@ -52,11 +22,7 @@ var ToasterComponent = function ToasterComponent(_ref) {
       return removeToast(toast.id);
     },
     className: "toast ".concat(toast.type, "_toast fade-in")
-  }, /*#__PURE__*/_react.default.createElement("img", {
-    className: "".concat(toast.type, "_icon"),
-    src: toastIcon,
-    alt: "".concat(toast.type, " icon")
-  }), /*#__PURE__*/_react.default.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "".concat(toast.type, "_text")
   }, /*#__PURE__*/_react.default.createElement("strong", null, toast.title), /*#__PURE__*/_react.default.createElement("br", null), toast.message, toast.refresh ? /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("br", null), /*#__PURE__*/_react.default.createElement("span", {
     role: "presentation",
